630ff8539b388a012ba408dba6090becd6f0075c
Junoway <wartyw@gmail.com>
2025-12-12 18:28:31 -0600

    fix: Add suppressHydrationWarning to session-filled form inputs

--- app/shop/[productId]/page.tsx
+++ app/shop/[productId]/page.tsx
@@ -808,6 +808,7 @@ export default function ProductPage({ params }: { params: Promise<{ productId: s
                     readOnly
                     className="w-full px-4 py-2 pr-10 border border-gray-300 rounded-lg bg-gray-100 text-gray-600 cursor-not-allowed"
                     tabIndex={-1}
+                    suppressHydrationWarning
                   />
                   <svg className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
@@ -830,6 +831,7 @@ export default function ProductPage({ params }: { params: Promise<{ productId: s
                     readOnly
                     className="w-full px-4 py-2 pr-10 border border-gray-300 rounded-lg bg-gray-100 text-gray-600 cursor-not-allowed"
                     tabIndex={-1}
+                    suppressHydrationWarning
                   />
                   <svg className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
